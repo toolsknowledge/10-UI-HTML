2f333f492f72658a4fe177f3cde33b6835fb014c
Sambasivarao <samba.pendela@gmail.com>
2022-01-22 19:32:24 +0530

    code

---
+++ JS/demo.js
@@ -0,0 +1,23 @@
+console.log("welcome to javascript");                   //welcome to javascript
+
+console.table(["Hello_1","Hello_2","Hello_3"]);
+/*
+┌─────────┬───────────┐
+│ (index) │  Values   │
+├─────────┼───────────┤
+│    0    │ 'Hello_1' │
+│    1    │ 'Hello_2' │
+│    2    │ 'Hello_3' │
+└─────────┴───────────┘
+*/
+
+
+console.log( 10+10 );                   //20
+console.log( 10+"10" );                 //1010
+console.log(10+ +"10");                 //20
+console.log( 10 - "10" );               //0
+console.log( "10" - "10" );             //0
+console.log( 10 / "10" );               //1
+console.log( 10 * "10" );               //100
+console.log( "10" / "0" );              //Infinity
+console.log( "10" / "A" );              //NaN
