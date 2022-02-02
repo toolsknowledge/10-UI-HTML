af2c2e1c5490cf055352f925475bcb2c7917ef09
Sambasivarao <samba.pendela@gmail.com>
2022-02-02 19:26:08 +0530

    code

--- JS/variables1.js
+++ JS/variables1.js
@@ -48,7 +48,7 @@ let data = 100;
 
 
 
-
+/*
 let x = 100;
 let x = 200;
 console.log(x);
@@ -57,6 +57,125 @@ console.log(x);
 
 //var keyword allows the duplicate variables
 //let keyword won't allows the duplicate variables
+*/
+
+
+
+
+/*
+                var                                                 let
+    **************************************************************************************************************
+    var keyword introduced in ES1                   let keyword introduced in ES6
+
+    var keyword allows the "duplicate"              let keyword won't allows the "duplicate" variables
+    variables    
+
+    variable "hoisting" raised because of           we can overcome variable hoisting with the help of
+    var keyword                                     let keyword
+
+    global polluting issue "raised"                 we can overcome global polluting issue with "let" keyword
+    because of var keyword
+
+    scope rule break by "var" keyword              scope rule obey by let keyword
+
+    var members called as "global" members         let members called as "block" scoped members
+    ************************************************************************************************************
+*/
+
+
+
+//const
+//const is the keyword
+//ES6
+//reinitilization not possible
+const data = 100;
+//data = 200;                                 //TypeError: Assignment to constant variable.
+
+
+const arr = [100,200,300,400,500];
+//arr = [];                                       //TypeError: Assignment to constant variable.
+//arr = [10,20,30,40,50];                         //TypeError: Assignment to constant variable.
+
+arr[0] = 10;
+arr[4] = 50;
+console.log(arr);                               //[ 10, 200, 300, 400, 50 ]
+
+
+const obj = {"key1":"value1","key2":"value2","key3":"value3"};
+//obj = {};               //TypeError: Assignment to constant variable.
+
+//obj = {"key1":"welcome1","key2":"welcome2","key3":"welcome3"};
+
+                    //TypeError: Assignment to constant variable.
+
+obj.key1 = "welcome_1";
+obj.key2 = "welcome_2";
+obj.key3 = "welcome_3";
+console.log(obj);               //{ key1: 'welcome_1', key2: 'welcome_2', key3: 'welcome_3' }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
