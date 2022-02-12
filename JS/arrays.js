113e23f3617ee8a1dee929fe134f01832e28fca2
Sambasivarao <samba.pendela@gmail.com>
2022-02-12 19:53:20 +0530

    arrays manipulations

--- JS/arrays.js
+++ JS/arrays.js
@@ -108,6 +108,283 @@
 */
 
 
+/*
+    console.log(
+        [1,2,3,4,5].map((element,index)=>{
+            return element*10;
+        })
+    );
+    //[ 10, 20, 30, 40, 50 ]
+
+
+
+    console.log(
+        [100,200,300,400,500].map((element,index)=>{
+            return element/100;
+        })
+    );
+    //[ 1, 2, 3, 4, 5 ]
+
+
+    console.log(
+        [1,2,3,4,5].map((element,index)=>{
+            return {[element]:index};
+        })
+    );
+    //[ [ 1, 0 ], [ 2, 1 ], [ 3, 2 ], [ 4, 3 ], [ 5, 4 ] ]
+    //[ { '1': 0 }, { '2': 1 }, { '3': 2 }, { '4': 3 }, { '5': 4 } ]
+*/
+
+
+
+/*
+    console.log(
+        [1,2,3,4,5].filter((element,index)=>{
+            return element>=4;
+        })
+    );
+    //[ 4, 5 ]
+
+    console.log(
+        [1,2,3,4,5].map((element,index)=>{
+            return element*100;
+        }).filter((element,index)=>{
+            return element>200;
+        })
+    );
+    //[ 300, 400, 500 ]
+*/
+
+
+
+
+/*
+    console.log(
+        [1,2,3,4,5].reduce((firstElement,nextElement)=>{
+            return firstElement+nextElement;
+        })
+    );
+    //15
+
+    console.log(
+        [1,2,3,4,5].map((element,index)=>{
+            return element*100;
+        }).filter((element,index)=>{
+            return element>=100;
+        }).reduce((firstElement,nextElement)=>{
+            return firstElement+nextElement;
+        })
+    );
+    //1500
+*/
+
+
+
+
+/*
+    console.log(
+        ["UI","to","Welcome"].reduceRight((firstElement,nextElement)=>{
+            return firstElement+" "+nextElement;
+        })
+    );
+    //Welcome to UI
+*/
+
+
+/*
+    //slice()
+    let arr1 = [10,20,30,40,50,60,70,80,90,100];
+    console.log( arr1.slice(4,7) );                         //[ 50, 60, 70 ]
+    console.log( arr1.slice(0,2) );                         //[ 10, 20 ]
+    console.log( arr1.slice(0,1) );                         //[ 10 ]
+    console.log( arr1.slice(9) );                           //[ 100 ]
+    console.log( arr1.slice(-2,-1) );                       //[ 90 ]
+    console.log( arr1.slice(-1) );                          //[ 100 ]
+    console.log( arr1.slice(-10,-9) );                      //[ 10 ]
+    console.log( arr1.slice(3,7) );                         //[ 40, 50, 60, 70 ]
+    console.log( arr1.slice(-7,-3));                        //[ 40, 50, 60, 70 ]
+*/
+
+
+/*
+
+//... 
+//spread operator
+let arr1 = [1,2,3,4,5];
+let arr2 = [...arr1,6,7,8,9,10];
+console.log(arr2);          //[1,2,3,4,5,6,7,8,9,10]
+
+
+//addition of arrays
+let x1 = [1,2,3,4,5];
+let y1 = [6,7,8,9,10];
+let z1 = [...x1,...y1];
+console.log(z1);
+
+
+*/
+
+let arr1 = [1,2,3,4];
+//[3,4,1,2]
+
+
+//[1,2]
+//[3,4]
+
+//[3,4,1,2]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
